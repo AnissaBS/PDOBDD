65eb436a93d563663077d05fb84a328f0e40528b
AnissaBS <139979527+AnissaBS@users.noreply.github.com>
2023-11-29 16:34:38 +0100

    Add files via upload

--- connexion.php
+++ connexion.php
@@ -6,7 +6,7 @@
     
     $sql = "select * from client";
     $rs_req = $cnx->query($sql);
-    while($donnees=$rs_req->fetch()){
+    while($donnees=$rs_req->fetch(PDO::FETCH_ASSOC)){
         echo '<pre>';
         print_r($donnees);
         echo '</pre>';
